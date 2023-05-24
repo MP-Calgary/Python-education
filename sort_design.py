4da03a0f92936e0c7563da251168454d7797a615
parkem <michael.parker@robotsandpencils.com>
2023-05-23 19:47:05 -0600

    Add sort design

---
+++ sort_design.py
@@ -0,0 +1,28 @@
+import os
+# clear the terminal 
+os.system('clear')
+
+def sort_array(array):
+    n = len(array)
+    if n <= 1:
+        return array
+    
+    for i_pivot in range(n - 1, 0, -1):
+        i_largest = 0
+        for i_check in range(1, i_pivot + 1):
+            if array[i_check] > array[i_largest]:
+                i_largest = i_check
+        
+        array[i_pivot], array[i_largest] = array[i_largest], array[i_pivot]
+        print("  ", array)
+    
+    return array
+
+
+# Example usage
+unsorted_array = [31, 72, 10, 32, 18, 95, 25, 50]
+print("original array", unsorted_array)
+print()
+sorted_array = sort_array(unsorted_array)
+print()
+print("sorted array: ", sorted_array)
