9674dc4f9d98f55d421efde572efe3717d62ee9b
parkem <michael.parker@robotsandpencils.com>
2023-06-03 21:31:35 -0600

    Added comments to how the sorts work

--- various_sorts.py
+++ various_sorts.py
@@ -2,6 +2,9 @@ import os
 os.system('clear')  # clear the terminal 
 import random
 
+# Bubble Sort is a simple comparison-based sorting algorithm. It works by repeatedly swapping adjacent elements 
+# if they are in the wrong order until the entire array is sorted. The largest (or smallest, depending on the sorting order) 
+# element "bubbles" to the end of the array in each pass. This process is repeated until the array is completely sorted.
 def bubble_sort(arr):
     n = len(arr)
     steps = 0
@@ -12,6 +15,10 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return steps
 
+# Selection Sort is a comparison-based sorting algorithm. It divides the input array into two parts: 
+# the sorted part and the unsorted part. Initially, the sorted part is empty. The algorithm repeatedly selects the smallest 
+# (or largest) element from the unsorted part and swaps it with the leftmost element of the unsorted part. 
+# This process continues until the entire array is sorted.
 def selection_sort(arr):
     n = len(arr)
     steps = 0
@@ -24,6 +31,9 @@ def selection_sort(arr):
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return steps
 
+# Insertion Sort is comparison-based sorting algorithm that builds the final sorted array
+# one element at a time. It starts with the second element and compares it with the elements before it, 
+# shifting the greater elements one position to the right. This process continues until the entire array is sorted.
 def insertion_sort(arr):
     n = len(arr)
     steps = 0
@@ -37,6 +47,10 @@ def insertion_sort(arr):
         arr[j + 1] = key
     return steps
 
+# Merge Sort is a divide-and-conquer algorithm that works by repeatedly dividing the input array into smaller halves, 
+# sorting them, and then merging them back together. It divides the array into two halves until each half contains 
+# only one element (which is inherently sorted). Then it merges the sorted halves by repeatedly comparing 
+# the elements and placing them in the correct order.
 def merge_sort(arr):
     steps = 0
     if len(arr) <= 1:
@@ -50,6 +64,7 @@ def merge_sort(arr):
     steps += left_steps + right_steps + merge_steps
     return merged, steps
 
+# needed for merge_sort
 def merge(left, right):
     merged = []
     steps = 0
@@ -69,6 +84,10 @@ def merge(left, right):
         right = right[1:]
     return merged, steps
 
+# Quick Sort is a divide-and-conquer algorithm. It selects a pivot element from the array and partitions the other 
+# elements into two sub-arrays, according to whether they are less than or greater than the pivot. 
+# The sub-arrays are then recursively sorted. The pivot element is positioned correctly in the final sorted array. 
+# This process is repeated for each sub-array until the entire array is sorted.
 def quick_sort(arr):
     if len(arr) <= 1:
         return arr, 0  # Return sorted array and 0 steps if array is already sorted
@@ -86,6 +105,7 @@ def quick_sort(arr):
     
     return left + middle + right, steps
 
+# needed for heap_sort
 def heapify(arr, n, i, steps):
     largest = i
     left = 2 * i + 1
@@ -100,6 +120,10 @@ def heapify(arr, n, i, steps):
         steps = heapify(arr, n, largest, steps)
     return steps
 
+# Heap Sort uses a binary heap data structure to sort elements. First, it builds a max heap 
+# (for sorting in ascending order) or a min heap (for sorting in descending order) from the input array. 
+# Then it repeatedly extracts the root element (the maximum or minimum element, depending on the heap type),
+# places it at the end of the sorted array, and restructures the heap. This process continues until the entire array is sorted.
 def heap_sort(arr):
     n = len(arr)
     steps = 0
@@ -111,6 +135,11 @@ def heap_sort(arr):
         steps += 1
     return steps
 
+# Counting Sort is a non-comparison-based sorting algorithm that works well when the range of input values is small. 
+# It works by counting the frequency of each distinct element in the input array, creating a count array. 
+# Then it modifies the count array to store the actual position of each element in the output array. 
+# Finally, it iterates through the input array, placing each element in 
+# its correct position in the output array based on the modified count array.
 def counting_sort(arr):
     n = len(arr)
     output = [0] * n
@@ -130,6 +159,10 @@ def counting_sort(arr):
         arr[i] = output[i]
     return arr, steps
 
+# Radix Sort is a non-comparison-based sorting algorithm. It sorts the elements by their individual digits or bits. 
+# It starts by sorting the elements based on the least significant digit (or bit), 
+# then proceeds to the next significant digit (or bit), and so on, until all digits or bits have been considered. 
+# Radix Sort can use Counting Sort or other stable sorting algorithms as a subroutine to sort each digit or bit.
 def radix_sort(arr):
     steps = 0
     max_value = max(arr)
@@ -139,6 +172,7 @@ def radix_sort(arr):
         exp *= 10
     return steps
 
+# needed for radix_sort
 def counting_sort_by_digit(arr, exp):
     n = len(arr)
     output = [0] * n
@@ -160,18 +194,20 @@ def counting_sort_by_digit(arr, exp):
         arr[i] = output[i]
     return steps
 
+# want to generate a random list of numbers that can be test the sorting algoriths
+# takes a paramter of how many numbers in the list.
+# for now, the list is betwen 1 and 500
 def generate_random_list(num_integers):
     random_list = []
     for _ in range(num_integers):
         random_list.append(random.randint(1, 500))
     return random_list
 
-# Example usage
 num_integers = 8  # Number of integers to generate in the set
 
 data = generate_random_list(num_integers)
 
-# Test data
+# Test data if want to fix to a pre-defined list
 # data = [31, 72, 10, 32, 18, 95, 25, 50]
 
 # original list
