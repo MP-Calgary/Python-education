de127dd8d81934a22f622010655a189bc8850686
parkem <michael.parker@robotsandpencils.com>
2023-06-05 21:45:08 -0600

    tweak sort design based on solution psuedocode

--- sort_design.py
+++ sort_design.py
@@ -9,16 +9,31 @@ def sort_array(array):
     for i_pivot in range(n - 1, 0, -1):
         i_largest = 0
         for i_check in range(1, i_pivot + 1):
+            # print("ipivot: ",i_pivot,"  i_largest: ", i_largest, "  i_check: ",i_check)
             if array[i_check] > array[i_largest]:
                 i_largest = i_check
         
-        array[i_pivot], array[i_largest] = array[i_largest], array[i_pivot]
+        if array[i_largest] > array[i_pivot]:
+            array[i_pivot], array[i_largest] = array[i_largest], array[i_pivot]
         # print("  ", array)
     
     return array
 
 # Example usage
-unsorted_array = [31, 72, 10, 32, 18, 95, 25, 50]
+# unsorted_array = [31, 72, 10, 32, 18, 95, 25, 50]
+# unsorted_array = [26, 6, 90, 55]
+
+import random
+def generate_random_list(num_integers):
+    random_list = []
+    for _ in range(num_integers):
+        random_list.append(random.randint(1, 500))
+    return random_list
+
+num_integers = 8  # Number of integers to generate in the set
+
+unsorted_array = generate_random_list(num_integers)
+
 print("original array: ", unsorted_array)
 print()
 sorted_array = sort_array(unsorted_array)
