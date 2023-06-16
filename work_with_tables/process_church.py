80f3a99807bac7bae6392cdf36e0aa4ef7356803
parkem <michael.parker@robotsandpencils.com>
2023-06-15 20:00:44 -0600

    Changes to group by rows

--- work_with_tables/process_church.py
+++ work_with_tables/process_church.py
@@ -3,25 +3,19 @@ import os
 os.system('clear')
 import sys
 
-# Add the directory containing the module to the module search path
+# Add the folder to the system path containing the path to where package tabulate exists
 sys.path.insert(0, '/opt/homebrew/lib/python3.11/site-packages')
 
 import pandas as pd
 from tabulate import tabulate
 
-# adding MP_learn folder to the system path (this folder has the Python file called useful_functions, where I can import display_table)
-sys.path.insert(0, '//Users/michaelparker/Dropbox (Personal)/MP_Python/MP_learn/')
-
-# from useful_functions import display_table
 from datetime import datetime
 
 current_datetime = datetime.now() # Get the current date and time
 formatted_datetime = current_datetime.strftime("%-d-%b-%Y %-I:%M:%S %p") # Format the date and time as d-mmm-yyyy h:mm:ss am/pm (without leading zero for the day or hour)
 print("")
 print("========================================")
-print("")
 print(formatted_datetime) # Print the formatted date and time
-print("")
 print("========================================")
 
 #  set which file to load
@@ -30,7 +24,8 @@ my_file = 'parkland_export.csv'
 if os.path.exists(my_file):
 
     # used for debugging, have a small set of fixed data
-    # excel_data = [
+    # ----
+    # sample_data = [
     # ["Arishenkoff, Mark", 45, "29-May-78", "Melchizedek", "Elder", "Active", "Regular", "M", "Parkland Ward"],
     # ["Bachynski, Jordan", 33, "06-Sep-89", "Melchizedek", "Elder", "Expired", "Regular", "M", "Parkland Ward"],
     # ["Baker, Don", 76, "11-Oct-46", "Melchizedek", "High Priest", "Active", "Regular", "M", "Parkland Ward"],
@@ -39,6 +34,11 @@ if os.path.exists(my_file):
     # ["Brown, Lane", 65, "05-Feb-58", "Melchizedek", "Elder", "Active", "Regular", "M", "Parkland Ward"],
     # ["Chihaluca, Admir", 41, "18-Aug-81", "Melchizedek", "Elder", "Active", "Regular", "M", "Parkland Ward"]
     # ]
+    # columns = ["Preferred Name", "Age", "Birth Date", "Priesthood", "Priesthood office",
+    #        "Temple Recommend Status", "Temple Recommend Type", "Gender", "Unit"]
+
+    # excel_data = pd.DataFrame(sample_data, columns=columns)
+    # ---- end of debugging data
 
     # import csv into a pandas dataframe
     excel_data = pd.read_csv(my_file)
@@ -64,78 +64,62 @@ if os.path.exists(my_file):
     # Print the total number of rows
     print(f"Total Rows: {len(table_data)}")
 
+    # Calculate counts of rows for age groups by 5
+    age_bins = range(0, sorted_data['Age'].max() + 6, 5)
+    age_groups = pd.cut(sorted_data['Age'], bins=age_bins, right=False)
+    age_counts = sorted_data.groupby(age_groups).size().reset_index(name='Count')
+
+    # Create age group labels aligned with the bins
+    age_group_labels = [f'{age_bins[i]}-{age_bins[i+1]-1}' for i in range(len(age_bins)-1)]
+    age_group_counts = age_counts.values.tolist()
+
+    # Print the counts for age groups
+    print('\nCounts of Rows for Age Groups:')
+    print(tabulate(age_group_counts, headers=['Age Group', 'Count'], tablefmt='pretty'))
+
+    # Calculate and print the total count of rows
+    total_count = age_counts['Count'].sum()
+    print(f"Total Count: {total_count}")
+    print("\n\n")
+
+    # ------------------
+    # Filter the DataFrame based on 'Gender', 'Priesthood', and 'Temple Recommend Status'
+    filtered_data = excel_data[
+        (excel_data['Gender'] == 'M') &
+        (excel_data['Priesthood'] == 'Melchizedek') &
+        (excel_data['Temple Recommend Status'] == 'Active')
+    ]
+
+    # Sort the filtered DataFrame by age
+    sorted_data = filtered_data.sort_values('Age')
+
+    # Convert the sorted DataFrame to a list of lists
+    table_data = sorted_data.values.tolist()
+
+    # Print the table using tabulate
+    print("Data where Temple Recommend Status = 'Active' ")
+    print(tabulate(table_data, headers=sorted_data.columns, tablefmt='pretty'))
+
+    # Print the total number of rows
+    total_rows = len(table_data)
+    print(f"Total Rows: {total_rows}")
+
+    # Calculate counts of rows for age groups by 5
+    age_bins = range(0, sorted_data['Age'].max() + 6, 5)
+    age_groups = pd.cut(sorted_data['Age'], bins=age_bins, right=False)
+    age_counts = sorted_data.groupby(age_groups).size().reset_index(name='Count')
+
+    # Create age group labels aligned with the bins
+    age_group_labels = [f'{age_bins[i]}-{age_bins[i+1]-1}' for i in range(len(age_bins)-1)]
+    age_group_counts = age_counts.values.tolist()
+
+    # Print the counts for age groups
+    print("Counts of Rows for Age Groups Temple Recommend Status = 'Active'")
+    print(tabulate(age_group_counts, headers=['Age Group', 'Count'], tablefmt='pretty'))
+
+    # Calculate and print the total count of rows
+    total_count = age_counts['Count'].sum()
+    print(f"Total Count where Temple Recommend Status = 'Active': {total_count}")
+
 else:
     print(f"File '{my_file}' does not exist.")
-
-
-
-# sheet_name = 'Harvest'
-# column_indices = [1, 2, 3, 4, 7]  # read columns 1, 2, 3, 4, and 7
-
-# # columns_data = [['Date', '2015/11/25', '2015/11/30', '2015/12/07', '2015/12/11', '2015/12/15'],
-# #                 ['Client', 'RandP', 'GOA', 'Robots and Pencils', 'AHS', 'Decisive Farming'],
-# #                 ['Project', 'r&p-robofactory-pm/ba guild', 'goa-rmas0049-firebans', 'sales-goa queens printer ministry', 'ahs-wait times-ios', 'decisive-scouting'],
-# #                 ['Project Code', '013', '066', '112', '040', '095'],
-# #                 ['Hours', 8.5, 6.5, 1.5, 3, 2]]
-
-# if os.path.exists(file_path):
-
-#     columns_data = read_excel_columns(file_path, sheet_name, column_indices)  # returns value into a list
-
-#     transposed_data = zip(*columns_data)  # Transpose the list
-
-#     # convert class zip to a list
-#     transposed_list = list(transposed_data)
-
-#     df = pd.DataFrame(transposed_list[1:], columns=transposed_list[0])
-#     # print("Pandas list")
-#     # print(df)
-#     print()
-#     print("List of first 5 rows in panda set")
-#     print(df.head())
-#     print()
-
-#     # want to summarize the data, so it prints total hours by project code
-#     hours_sum = df.groupby(['Project Code', 'Project'])['Hours'].sum().reset_index()
-#     hours_sum_sorted = hours_sum.sort_values('Hours', ascending=False)
-
-#     num_unique_project_codes = len(hours_sum_sorted)
-#     output_list = []
-
-#     for index, row in hours_sum_sorted.iterrows():
-#         project_code = row['Project Code']
-#         project = row['Project']
-#         total_hours = row['Hours']
-#         output_list.append((project_code, project, total_hours))
-
-#     print("Code      Project                                 Total hours")
-#     display_table(output_list)
-#     print("Number of Unique Project Codes:", num_unique_project_codes)
-#     print()
-
-#     # Extract year from 'Date' column
-#     df['Year'] = pd.to_datetime(df['Date']).dt.year
-
-#     # Group by year and project code
-#     grouped = df.groupby(['Year', 'Project Code'])
-
-#     print("List out Project codes broken out by year")
-#     print()
-
-#     prev_year = None
-#     for (year, project_code), group in grouped:
-#         if year != prev_year:
-#             if prev_year is not None:
-#                 print()  # Print newline before starting the next year
-#             print(f"Year: {year}")
-#             prev_year = year
-
-#         project_names = group['Project'].unique()
-#         project_list = ', '.join(project_names)
-#         total_hours = group['Hours'].sum()
-#         print(f"Project Codes: {project_code}, Projects: {project_list}, Total Hours: {total_hours}")
-
-#     print()  # Print newline after the last year
-
-# else:
-#     print("File: ", file_path, " does not exist.")
