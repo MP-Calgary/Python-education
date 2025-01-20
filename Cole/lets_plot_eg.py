96e03e2ae7d78d88c8539abeb546b8145c74d13e
MP-Calgary <michaeljamesparker@gmail.com>
2025-01-20 14:45:45 -0700

    Update after testing

--- Cole/lets_plot_eg.py
+++ Cole/lets_plot_eg.py
@@ -1,8 +1,8 @@
-import lets_plot
-print(lets_plot.__version__)
+# import lets_plot
+# print(lets_plot.__version__)
 
-import sys
-print(sys.executable)
+# import sys
+# print(sys.executable)
 
 
 # from lets_plot import *
@@ -17,3 +17,36 @@ print(sys.executable)
 
 # # Create and display a scatter plot
 # ggplot(df, aes('x', 'y')) + geom_point(color='red', size=5) + ggtitle("Scatter Plot Example")
+
+######
+#  below here
+# from lets_plot import *
+# import pandas as pd
+
+# # Initialize Lets-Plot for non-Jupyter environments
+# LetsPlot.setup_html()
+
+# # Create sample data
+# data = {'x': [1, 2, 3, 4, 5], 'y': [3, 7, 8, 5, 9]}
+# df = pd.DataFrame(data)
+
+# # Create and display a scatter plot
+# ggplot(df, aes('x', 'y')) + geom_point(color='red', size=5) + ggtitle("Scatter Plot Example")
+
+###### 3rd try
+from lets_plot import *
+import pandas as pd
+
+# Initialize Lets-Plot for Jupyter
+LetsPlot.setup_jupyter()
+
+# Create sample data
+data = {'x': [1, 2, 3, 4, 5], 'y': [3, 7, 8, 5, 9]}
+df = pd.DataFrame(data)
+
+# Create and display a scatter plot
+plot = ggplot(df, aes('x', 'y')) + geom_point(color='red', size=5) + ggtitle("Scatter Plot Example")
+
+# Display the plot inline (only necessary if it does not automatically show)
+plot.show()
+
